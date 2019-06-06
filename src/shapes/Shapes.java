a8f022c9e70cebc32e2c701fa8b33d2e86a6e74a
Cheng Thao <cthao@csp.edu>
2019-06-05 20:39:58 -0500

    initial commit

---
+++ src/shapes/Shapes.java
@@ -0,0 +1,16 @@
+package shapes;
+
+/**
+ *
+ * @author Cheng Thao
+ */
+public class Shapes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
